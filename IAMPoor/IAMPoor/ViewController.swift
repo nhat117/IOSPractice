ffc4d558a80ae5f47ad262e2204177efb0d8923c
nhat117 <s3878174@rmit.edu.vn>
2022-05-25 07:07:38 +0700

    Add Files

---
+++ IAMPoor/IAMPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAMPoor
+//
+//  Created by Nhat Bui Minh on 21/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+
+func greeting2 (whoToGreet : String){
+    print("Hello \(whoToGreet)")
+}
+
+greeting2(whoToGreet: "Nhat")
+
