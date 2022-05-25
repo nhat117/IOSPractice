89c2cda680efe1a1cfb3106429aed2d1548a969e
nhat117 <s3878174@rmit.edu.vn>
2022-05-25 21:44:37 +0700

    Add new App QuizBuidler

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,3 @@
 import UIKit
 
-//var greeting = "Hello, playground"
-
-func greeting2 (whoToGreet : String){
-    print("Hello \(whoToGreet)")
-}
-
-greeting2(whoToGreet: "Nhat")
-
+var greeting = "Hello, playground"
