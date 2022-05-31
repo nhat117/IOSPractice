e4e021d46fa7a5690ff5f72c63930108c9b21392
nhat117 <s3878174@rmit.edu.vn>
2022-05-31 21:54:55 +0700

    Build 3 new ios

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,34 +1,3 @@
 import UIKit
 
-struct MyOptional {
-    let k = 4
-    let z = 2
-    
-    func res() {
-        print("Hello")
-    }
-}
-
-var greeting : MyOptional?
-
-
-greeting = MyOptional()
-
-//Optional binding
-
-//if let msg = greeting {
-//    print(msg)
-//} else {
-//    print("Greeting is nil")
-//}
-
-
-//Coelessing operator
-
-
-
-let text : Int = greeting?.k ?? 1
-
-print(text)
-
-greeting?.res()
+var greeting = "Hello, playground"
