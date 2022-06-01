29fd1529262f87c5e6dbf432941e8860e4065883
nhat117 <s3878174@rmit.edu.vn>
2022-06-02 00:07:54 +0700

    Add FlashChat

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let contacts = ["John":123, "Jessi" : 456,"Ethan":789,"Christina": 1020]
+
+let halfOpenRange = 1..<5
+
+let fullOpenRange = 1...5
+
+var now = Date().timeIntervalSince1970
+
+let oneSec = now + 1
+
+while now < oneSec {
+    print("Waiting....")
+    now = Date().timeIntervalSince1970
+}
+
+for person in contacts {
+    print(person.key)
+    
+}
