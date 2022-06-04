275fc5c279e774c0d575164fb02c1e78c982587d
nhat117 <s3878174@rmit.edu.vn>
2022-06-04 09:45:20 +0700

    Add FlashChat

---
+++ SwiftUI/IAmRich/IAmRich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IAmRich
+//
+//  Created by Nhat Bui Minh on 04/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/IAmRich/IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Nhat Bui Minh on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
